16bfac107a1dc182d7170ce71835fbb086bf6fef
Erik Ecoologic <erikecoologic@gmail.com>
2025-12-28 12:45:04 +1000

    chore: Keep MIT licence even in private repos

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -6,15 +6,6 @@ if [[ -n $(git status --porcelain) ]]; then
   exit 1
 fi
 
-# Remove MIT license
-sed -i '' '/"license": "MIT",/d' package.json
-sed -i '' '/"license": "MIT",/d' packages/ui/package.json
-sed -i '' '/"license": "MIT",/d' packages/auth/package.json
-sed -i '' '/"license": "MIT",/d' packages/db/package.json
-sed -i '' '/"license": "MIT",/d' packages/api/package.json
-sed -i '' '/"license": "MIT",/d' packages/validators/package.json
-rm LICENSE
-
 # Prep the readme for _this_ project
 rm README.md
 rm LABRYS-README.md
